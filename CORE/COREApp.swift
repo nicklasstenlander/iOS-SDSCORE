aff79d203b20237b255b7ac3734e123592ee8925
Nicklas Stenlander Marquart <nicklas.stenlander@me.com>
2026-07-05 15:14:16 +0200

    Initial Commit

---
+++ CORE/COREApp.swift
@@ -0,0 +1,17 @@
+//
+//  COREApp.swift
+//  CORE
+//
+//  Created by Nicklas Stenlander Marquart on 2026-07-05.
+//
+
+import SwiftUI
+
+@main
+struct COREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
